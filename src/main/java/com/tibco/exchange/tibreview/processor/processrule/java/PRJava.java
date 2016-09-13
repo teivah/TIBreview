14aea96d71fb31b30375a4c7230c82622f79c486
teiva harsanyi <harsanta@skysoft-atm.com>
2016-09-13 23:18:03 +0200

    Java process rule

---
+++ src/main/java/com/tibco/exchange/tibreview/processor/processrule/java/PRJava.java
@@ -0,0 +1,7 @@
+package com.tibco.exchange.tibreview.processor.processrule.java;
+
+import com.tibco.exchange.tibreview.processor.processrule.PRProcessable;
+
+public abstract class PRJava implements PRProcessable {
+
+}
---
+++ src/main/java/com/tibco/exchange/tibreview/processor/processrule/java/PRJavaTest.java
@@ -0,0 +1,20 @@
+package com.tibco.exchange.tibreview.processor.processrule.java;
+
+import java.util.List;
+
+import com.tibco.exchange.tibreview.common.TIBProcess;
+import com.tibco.exchange.tibreview.engine.Context;
+import com.tibco.exchange.tibreview.exception.ProcessorException;
+import com.tibco.exchange.tibreview.model.pmd.Violation;
+import com.tibco.exchange.tibreview.model.rules.Rule;
+
+public class PRJavaTest extends PRJava {
+
+	@Override
+	public List<Violation> process(Context context, TIBProcess process, Rule rule, Object impl)
+			throws ProcessorException {
+		System.out.println("Java rule: context=" + context + ", process=" + process + ", rule=" + rule + ", impl=" + impl);
+		return null;
+	}
+
+}
